ea3f3f37e7975b91579e74553e0b53f4d350f798
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-07-22 15:50:58 -0300

    criacao do componente posto-saude.

---
+++ src/app/posto-saude/posto-saude.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-posto-saude',
+  templateUrl: './posto-saude.component.html',
+  styleUrls: ['./posto-saude.component.css']
+})
+export class PostoSaudeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
