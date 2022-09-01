0f9be7222f3c910c5363dca10396a4d2b387ae56
Marcelo Piano <marcelopianopatuscosantiago@gmail.com>
2022-09-01 16:46:15 -0300

    feat: add header and pagina-login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaLoginComponent } from './pagina-login/pagina-login.component';
+import { PostoSaudeComponent } from './posto-saude/posto-saude.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: 'posto', component: PostoSaudeComponent},
+  {path: 'login', component: PaginaLoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostoSaudeComponent } from './posto-saude/posto-saude.component';
+import { PaginaLoginComponent } from './pagina-login/pagina-login.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostoSaudeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PostoSaudeComponent, PaginaLoginComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
