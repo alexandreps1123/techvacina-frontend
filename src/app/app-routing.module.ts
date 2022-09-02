ccc9a09e065f1eb20212742e383bbccaca94b209
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-09-01 23:33:45 -0300

    retirado a chamda de alguns componentes e implementado o serviço de redirecionamento.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  {
+    path: 'usuario',
+    loadChildren: () => import('./usuario/usuario-routing.module').then((m) => m.UsuarioRoutingModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 import { PostoSaudeComponent } from './posto-saude/posto-saude.component';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostoSaudeComponent
+    PostoSaudeComponent,
+    CabecalhoComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    UsuarioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
